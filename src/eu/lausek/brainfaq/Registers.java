50f7396d0fa95829cd29db2b5acaa642cc640927
lausek <root@lausek.eu>
2017-11-02 08:14:58 +0100

    moved Cell to Registers class

--- src/eu/lausek/brainfaq/Registers.java
+++ src/eu/lausek/brainfaq/Registers.java
@@ -76,3 +76,25 @@ public class Registers {
 	}
 
 }
+
+class Cell {
+	
+	private int value = 0;
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public void increment() {
+		value++;
+	}
+	
+	public void decrement() {
+		value--;
+	}
+	
+}
